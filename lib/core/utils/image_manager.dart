0abe4b2f79f7a77efb8ff36bef5d34a440c82072
omarhisham55 <omarhishamho@gmail.com>
2024-02-28 12:12:16 +0200

    fix imgs

--- lib/core/utils/image_manager.dart
+++ lib/core/utils/image_manager.dart
@@ -1,60 +1,70 @@
-const baseImgPath = 'assets/images';
-const splashPath = '$baseImgPath/splash_screen';
-const settingsPath = '$baseImgPath/settings';
-const profileSetupPath = '$baseImgPath/profile';
-const profilehealthPath = '$baseImgPath/profile/health';
-const profileNutritionPath = '$baseImgPath/profile/nutrition';
-const profileActivitiesPath = '$baseImgPath/profile/activities';
+import 'package:pet_app/config/preferences/shared_preferences.dart';
+
+const baseLightImgPath = 'assets/images/lightImages';
+const baseDarkImgPath = 'assets/images/darkImages';
+
+String themeImage(String folderPath) {
+  return LocalSharedPreferences.darkTheme()
+      ? '$baseDarkImgPath/$folderPath'
+      : '$baseLightImgPath/$folderPath';
+}
+
+final String splashPath = themeImage('splash_screen');
+final String settingsPath = themeImage('settings');
+final String profileSetupPath = themeImage('profile');
+final String profilehealthPath = themeImage('profile/health');
+final String profileNutritionPath = themeImage('profile/nutrition');
+final String profileActivitiesPath = themeImage('profile/activities');
 
 class SplashImages {
-  static const String logo = '$splashPath/logo.png';
-  static const String background = '$splashPath/bg.png';
+  static final String logo = '$splashPath/logo.png';
+  static final String background = '$splashPath/bg.png';
 }
 
 class ProfileImages {
-  static const String noProfileSetup = '$profileSetupPath/no_profile_setup.png';
-  static const String healthCard = '$profileSetupPath/health_card.png';
-  static const String nutrition = '$profileSetupPath/nutrition.png';
-  static const String activities = '$profileSetupPath/activities.png';
-  static const String emptyInsurance = '$profileSetupPath/empty_insurance.png';
-  static const String contactBg = '$profileSetupPath/contactBg.png';
-  static const String haircutSalon = '$profileSetupPath/haircutSalon.png';
+  static final String noProfileSetup = '$profileSetupPath/no_profile_setup.png';
+  static final String healthCard = '$profileSetupPath/health_card.png';
+  static final String nutrition = '$profileSetupPath/nutrition.png';
+  static final String activities = '$profileSetupPath/activities.png';
+  static final String emptyInsurance = '$profileSetupPath/empty_insurance.png';
+  static final String contactBg = '$profileSetupPath/contactBg.png';
+  static final String haircutSalon = '$profileSetupPath/haircutSalon.png';
 }
 
 class HealthImages {
-  static const String insurance = '$profilehealthPath/insurance.png';
-  static const String vaccines = '$profilehealthPath/vaccines.png';
-  static const String antiParasiticalTreatment =
+  static final String insurance = '$profilehealthPath/insurance.png';
+  static final String vaccines = '$profilehealthPath/vaccines.png';
+  static final String antiParasiticalTreatment =
       '$profilehealthPath/anti-parasitical_treatment.png';
-  static const String medicalIntervention =
+  static final String medicalIntervention =
       '$profilehealthPath/medical_intervention.png';
-  static const String otherTreatment = '$profilehealthPath/other_treatment.png';
+  static final String otherTreatment = '$profilehealthPath/other_treatment.png';
 }
 
 class NutritionImages {
-  static const List<String> recipe = [
+  static final List<String> recipe = [
     '$profileNutritionPath/recipe1.png',
     '$profileNutritionPath/recipe2.png',
     '$profileNutritionPath/recipe3.png'
   ];
-  static const String recipes = '$profileNutritionPath/recipes.png';
-  static const String dogFood = '$profileNutritionPath/dog_food.png';
-  static const String homeCooked = '$profileNutritionPath/home_cooked.png';
-  static const String kibbleDry = '$profileNutritionPath/kibble_dry.png';
+  static final String recipes = '$profileNutritionPath/recipes.png';
+  static final String dogFood = '$profileNutritionPath/dog_food.png';
+  static final String homeCooked = '$profileNutritionPath/home_cooked.png';
+  static final String kibbleDry = '$profileNutritionPath/kibble_dry.png';
 }
 
 class ActivitiesImages {
-  static const String handshake = '$profileActivitiesPath/handshake.png';
-  static const String sit = '$profileActivitiesPath/sit.png';
+  static final String handshake = '$profileActivitiesPath/handshake.png';
+  static final String sit = '$profileActivitiesPath/sit.png';
 }
 
 class SettingsImages {
-  static const String timeZone = '$settingsPath/timeZone.png';
-  static const String language = '$settingsPath/language.png';
-  static const String darkMode = '$settingsPath/darkMode.png';
-  static const String locationAccess = '$settingsPath/locationAccess.png';
-  static const String photoAccess = '$settingsPath/photoAccess.png';
-  static const String appNotifications = '$settingsPath/appNotifications.png';
-  static const String emailNotifications =
+  static final String timeZone = '$settingsPath/timeZone.png';
+  static final String language = '$settingsPath/language.png';
+  static final String darkMode = '$settingsPath/darkMode.png';
+  static final String locationAccess = '$settingsPath/locationAccess.png';
+  static final String photoAccess = '$settingsPath/photoAccess.png';
+  static final String appNotifications = '$settingsPath/appNotifications.png';
+  static final String emailNotifications =
       '$settingsPath/emailNotifications.png';
 }
